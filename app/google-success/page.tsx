1f4874b02289eb93a94c79e46f6b8aec32ec2987
aluruabhi <Abhilash@superu.ai>
2026-03-12 15:19:23 +0530

    server wrapper for googlesuccessclient

--- app/google-success/page.tsx
+++ app/google-success/page.tsx
@@ -1,27 +1,7 @@
-"use client";
+import GoogleSuccessClient from "@/components/GoogleSuccessClient";
+
 export const dynamic = "force-dynamic";
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-
-export default function GoogleSuccess() {
-  const router = useRouter();
-  const params = useSearchParams();
-
-  useEffect(() => {
-    const token = params.get("token");
-
-    if (token) {
-      localStorage.setItem("token", token);
-      router.push("/dashboard");
-    } else {
-      router.push("/login");
-    }
-  }, [params, router]);
-
-  return (
-    <p className="text-xl font-semibold text-gray-500 animate-pulse w-full h-screen flex justify-center items-center">
-      Signing you in...
-    </p>
-  );
+export default function Page() {
+  return <GoogleSuccessClient />;
 }
---
+++ components/GoogleSuccessClient.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+
+export default function GoogleSuccessClient() {
+  const router = useRouter();
+  const params = useSearchParams();
+
+  useEffect(() => {
+    const token = params.get("token");
+
+    if (token) {
+      localStorage.setItem("token", token);
+      router.push("/dashboard");
+    } else {
+      router.push("/login");
+    }
+  }, [params, router]);
+
+  return (
+    <p className="text-xl font-semibold text-gray-500 animate-pulse w-full h-screen flex justify-center items-center">
+      Signing you in...
+    </p>
+  );
+}
